18def8ad46e1fc4704ba563e48fe2d50b08bed13
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2022-04-11 18:17:55 +0200

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -5,5 +5,9 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class ClientHandler implements Runnable {
-
+      public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+      private Socket socket;
+      private BufferedReader bufferedReader;
+      private BufferedWriter bufferedWriter;
+      private String clientUsername;
 }
