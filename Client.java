85d2f72ffd15846b751c98bec13ab278a8872d53
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2022-04-11 16:08:42 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,6 +5,13 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+   
+   private Socket socket;
+   private BufferedReader bufferedReader;
+   private BufferedWriter bufferedWriter;
+   private String username;
+   
+   
    public static void main (String [] args){
    
    }
